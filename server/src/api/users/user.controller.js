860cbdba1db1ef5db3302d1f5cb33988a1542912
san9w9n <sw994812@hanyang.ac.kr>
2022-02-15 20:07:40 +0900

    디렉토리 틀 잡기

--- server/src/api/users/user.controller.js
+++ server/src/api/users/user.controller.js
@@ -14,30 +14,6 @@ class UserController {
   initializeRoutes() {
     this.router.get('/signup', this.signup).post('/signup', this.signup)
   }
-
-  signup(req, res) {
-    if (req.method === 'GET') {
-      res.send('signup')
-      return
-    }
-    const { id, pw, name, email } = req.body
-    if (!id || !pw || !name || !email) {
-      res.send('Error')
-      return
-    }
-    UserRepository.create([id, pw, name, email])
-  }
-
-  signin(req, res) {
-    if (req.method === 'GET') {
-      res.send('signin')
-      return
-    }
-    const { ID, PW } = req.body
-    if (!ID || !PW) {
-      // res.send('Error')
-    }
-  }
 }
 
 module.exports = UserController
--- server/src/api/users/user.repository.js
+++ server/src/api/users/user.repository.js
@@ -5,14 +5,6 @@ const { db } = require('../../lib/database')
 
 class UserRepository {
   static create(raw) {
-    const query = {
-      text: 'INSERT INTO public.user(id, pw, name, email) VALUES($1, $2, $3, $4)',
-      values: raw,
-    }
-    db.query(query, (err, res) => {
-      if (err) throw new Error('User insert error.')
-      else console.log(`User insert successfully. (${res.rows[0]})`)
-    })
   }
 }
 
