e6bb266af4d6a8ed1ae9ca7a224cea70f1c0660e
san9w9n <sw994812@hanyang.ac.kr>
2022-03-03 20:47:03 +0900

    email  보내는 것 까지 성공

--- src/api/users/user.controller.js
+++ src/api/users/user.controller.js
@@ -1,21 +1,55 @@
 /* eslint-disable class-methods-use-this */
 
 const { Router } = require('express')
+const nodemailer = require('nodemailer')
 
 class UserController {
   constructor() {
-    this.path = '/user'
+    this.path = '/users'
     this.router = Router()
 
     this.initializeRoutes()
   }
 
+  // 이메일 인증 => 성공 => 비밀번호, 이름, ...
+
   initializeRoutes() {
-    this.router.get('/signup', this.signup).post('/signup', this.signup)
+    this.router.post('/email/auth', this.emailAuth)
   }
 
-  signup(req, res) {
-    res.json({ good: 'good' })
+  emailAuth(req, res) {
+    console.log(req.body)
+    const { email } = req.body
+    if (!email) {
+      res.json({
+        success: false,
+        message: 'NO EMAIL INFO.',
+      })
+      return
+    }
+
+    const mailConfig = {
+      service: 'Naver',
+      host: 'smtp.naver.com',
+      port: 587,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PW,
+      },
+    }
+
+    const message = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: '[Hands up] 이메일 인증 요청 메일입니다.',
+      html: '<p> 여기에 인증번호나 token 검증 URL 붙이시면 됩니다! </p>',
+    }
+    const transporter = nodemailer.createTransport(mailConfig)
+    transporter.sendMail(message)
+    res.json({
+      success: true,
+      message: 'NO EMAIL INFO.',
+    })
   }
 }
 
